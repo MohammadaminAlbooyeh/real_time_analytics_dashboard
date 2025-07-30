caa811bbc7ba3c61cc718e07de6fbd475ac9178f
Amin Albooyeh <amin.albooyeh@gmail.com>
2025-07-30 08:44:12 +0200

    create boxes page

--- cards/urls.py
+++ cards/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path
+from django.views.generic import TemplateView 
 from .views import (
-    CardListView,
     CardCreateView,
     CardUpdateView,
-    CardDeleteView
+    CardDeleteView,
+    BoxView,
+    CardListView,
 )
 
 urlpatterns = [
-    path('', CardListView.as_view(), name='card-list'),
+    path('', TemplateView.as_view(template_name='cards/home.html'), name='home'), 
     path('new/', CardCreateView.as_view(), name='card-create'),
     path('<int:pk>/update/', CardUpdateView.as_view(), name='card-update'),
     path('<int:pk>/delete/', CardDeleteView.as_view(), name='card-delete'),
+    path('boxes/', BoxView.as_view(), name='boxes'),
+    path('boxes/<int:box_num>/', CardListView.as_view(), name='box-cards'),
 ]
