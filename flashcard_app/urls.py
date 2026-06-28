862128ec03396b18362c143ad62b1065043a3c04
Amin Albooyeh <amin.albooyeh@gmail.com>
2026-06-28 10:27:17 +0200

    replace flashcard app with real-time analytics dashboard

--- flashcard_app/urls.py
+++
@@ -1,9 +0,0 @@
-# flashcard_app/flashcard_app/urls.py
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('cards.urls')),
-]
